f4bb10da5f402d99fac3f66843f658db92e604d7
kostik700015 <kostik700015@gmail.com>
2021-06-01 18:52:25 -0400

    OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/oders', to: 'oders#index'
+  post '/orders', to: 'orders#create'
+  get '/orders/new', to: 'orders#new', as: 'new_order' 
+  get '/orders/:id', to: 'orders#show', as: 'order'  
 end
